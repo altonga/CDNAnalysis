01cf8f082c73030aced360b2183273747d7d5d9d
Alok Tongaonkar <Alok_Tongaonkar@symantec.com>
2016-07-12 13:20:55 -0700

    Synthetic changes to clean up code

--- cdnanalysis.py
+++ cdnanalysis.py
@@ -1,8 +1,8 @@
-"""
+'''
 CDN Analysis
 
 python cdnanalysis.py -i top-1m.csv -c cdnproviders.csv -o opcdn -t 500
-"""
+'''
 import sys
 import getopt
 import os
@@ -24,7 +24,7 @@ def get_timing(cdn_domain_list, ofile):
     successfully, then run the experiments num_experiments times.
     Store details of each run of experiments in pretty formatted table as .txt
     file. Store the min, mean, max information per CDN in .csv file.
-    Display the same information as box plots in .png
+    Display the same information as box plots in .png file.
     '''
     prefixes = ['http://', 'http://www.', 'https://', 'https://www.']
     df_columns = ['CDN', 'Domain', 'URL', 'Namelookup', 'Connect',
@@ -35,7 +35,7 @@ def get_timing(cdn_domain_list, ofile):
             for prefix in prefixes:
                 url = prefix + domain
                 try:
-                    print "Trying: " + url
+                    print 'Trying: ' + url
                     c_output = check_output(['curl', '-s', '-o', '/dev/null',
                                              '-H', '"Pragma:no-cache"',
                                              '-H', '"Cache-Control: no-cache"',
@@ -50,7 +50,7 @@ def get_timing(cdn_domain_list, ofile):
                     times = c_output.split(',')
                     status_code = times[0]
                     if (status_code.startswith('2')):
-                        print "Success " + status_code + ": " + url
+                        print 'Success ' + status_code + ': ' + url
                         for x in range(num_experiments):
                             c_str = check_output(['curl', '-s', '-o',
                                                   '/dev/null',
@@ -64,21 +64,21 @@ def get_timing(cdn_domain_list, ofile):
                                                   '%{time_pretransfer},'
                                                   '%{time_starttransfer},'
                                                   '%{time_total}'])
-                            dfstring = cdn_name + "," + domain + "," + url \
-                                + "," + c_str
+                            dfstring = cdn_name + ',' + domain + ',' + url \
+                                + ',' + c_str
                             text = StringIO(dfstring)
                             newdf = pd.read_csv(text, names=df_columns)
                             df = df.append(newdf, ignore_index=True)
                         break
                     elif (status_code.startswith('3')):
-                        print "Redirected " + status_code + ": " + url
+                        print 'Redirected ' + status_code + ': ' + url
                     elif (status_code.startswith('4')):
-                        print "Error " + status_code + ": " + url
+                        print 'Error ' + status_code + ': ' + url
                     else:
-                        print "Unhandled status code " + status_code + ": " \
+                        print 'Unhandled status code ' + status_code + ': ' \
                               + url
                 except:
-                    print "Execption in: " + url
+                    print 'Execption in: ' + url
 
         '''
         It is not clear whether in curl output, connect time includes
@@ -91,12 +91,12 @@ def get_timing(cdn_domain_list, ofile):
         df['SSL'] = df['Appconnect'] - df['Connect']
         df['TTFB'] = df['Starttransfer']
         '''
-    with open(ofile+".txt", 'w') as outputfile:
+    with open(ofile+'.txt', 'w') as outputfile:
         outputfile.write(tabulate(df, headers='keys', tablefmt='psql'))
 
     df2 = df.groupby(['CDN', 'Domain', 'URL']).agg([np.min, np.mean, np.max])
-    output_csv = ofile + ".csv"
-    df2.to_csv(output_csv, mode="w")
+    output_csv = ofile + '.csv'
+    df2.to_csv(output_csv, mode='w')
     plt.figure()
     df.boxplot(column=['Namelookup', 'Connect', 'Appconnect',
                        'Starttransfer'], by='CDN')
@@ -130,30 +130,32 @@ def is_cdn(domain, cdnList):
 def get_cdn_domains(domain, cdnList):
     '''
     Run dig and check if either the domain name or the domains
-    returned as CNAME are associated with well-known CDNS
+    returned as CNAME are associated with well-known CDNS.
     '''
     endl = os.linesep
-    digOutput = check_output(["dig", domain, dns_server])
-    prettyDigOutput = digOutput.split(endl)
-    (iscdn, cdnname) = is_cdn(domain, cdnList)
-    if iscdn:
-        print "CDN Domain: ", domain
-        return cdnname
-    else:
-        for line in prettyDigOutput:
-            if 'CNAME' in line:
-                (iscdn, cdnname) = is_cdn(line, cdnList)
-                if iscdn:
-                    print "CDN CNAME: ", line, domain
-                    return cdnname
-
-    return ""
+    try:
+        dig_output = check_output(['dig', domain, dns_server])
+        pretty_dig_output = dig_output.split(endl)
+        (iscdn, cdnname) = is_cdn(domain, cdnList)
+        if iscdn:
+            print 'CDN Domain: ', domain
+            return cdnname
+        else:
+            for line in pretty_dig_output:
+                if 'CNAME' in line:
+                    (iscdn, cdnname) = is_cdn(line, cdnList)
+                    if iscdn:
+                        print 'CDN CNAME: ', line, domain
+                        return cdnname
+    except:
+        print 'Exception in dig for: ' + domain
+    return ''
 
 
 def get_top_sites(ifile, threshold):
     '''
     Get top threshold number of sites from the file containing
-    top sites from Alexa
+    top sites from Alexa.
     '''
     endl = os.linesep
 
@@ -165,7 +167,7 @@ def get_top_sites(ifile, threshold):
 
 
 def usage(pgm):
-    """Print usage message"""
+    '''Print usage message.'''
     print 'Usage: %s -i <inputfile> -c <cdnfile> -o <outputfile> ' \
           '-t <threshold>' % pgm
 
@@ -176,7 +178,7 @@ def main(argv):
     outputfile = ''
     threshold = 0
 
-    """
+    '''
     Parse command line option
 
     * inputfile: csv file containing the alexa top 1M sites/domains
@@ -184,12 +186,12 @@ def main(argv):
     * outputfile: txt file containing output of cdn analysis, a csv file is
     *             also generated with same prefix showing averages
     * threshold: number of top sites/domains to consider
-    """
+    '''
     try:
         opts, args = getopt.getopt(argv[1:],
-                                   "hi:c:o:t:",
-                                   ["ifile=", "cfile=", "ofile=",
-                                    "threshold="])
+                                   'hi:c:o:t:',
+                                   ['ifile=', 'cfile=', 'ofile=',
+                                    'threshold='])
     except getopt.GetoptError:
         usage(argv[0])
         sys.exit(2)
@@ -197,19 +199,19 @@ def main(argv):
         if opt == '-h':
             usage(arg[0])
             sys.exit()
-        elif opt in ("-i", "--ifile"):
+        elif opt in ('-i', '--ifile'):
             inputfile = arg
-        elif opt in ("-c", "--cfile"):
+        elif opt in ('-c', '--cfile'):
             cdnfile = arg
-        elif opt in ("-o", "--ofile"):
+        elif opt in ('-o', '--ofile'):
             outputfile = arg
-        elif opt in ("-t", "--threshold"):
+        elif opt in ('-t', '--threshold'):
             threshold = int(arg)
 
     print 'Input file is ', inputfile
     print 'CDN file is ', cdnfile
     print 'Output file is ', outputfile
-    print 'Threshold is ', str(threshold), "\n"
+    print 'Threshold is ', str(threshold), '\n'
     if inputfile == '' or cdnfile == '' or outputfile == '' or threshold <= 0:
         usage(argv[0])
         sys.exit(2)
@@ -225,10 +227,10 @@ def main(argv):
     sorted_cdn_domain_list = sorted(cdn_domain_list, key=lambda t: len(t[1]),
                                     reverse=True)
 
-    print "\n{CDN: DomainName}"
+    print '\n{CDN: DomainName}'
     print sorted_cdn_domain_list
-    print "\n"
+    print '\n'
     get_timing(sorted_cdn_domain_list, outputfile)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[:])
